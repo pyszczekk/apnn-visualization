fe99852e1eedb5636e58b9cec8ac53e6c25784bf
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2022-08-25 19:44:22 +0200

    example update

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,14 @@ import { VisualizationApp } from 'apnn-visualization'
 import 'apnn-visualization/dist/index.css'
 
 const App = () => {
-  return <VisualizationApp />
+  return <VisualizationApp 
+  connectionAddress = {"192.168.15.15:13001"} 
+  bgColor = {"#E9DEE9"} 
+  neuronColor = {"#F4D35E"}
+  synapsisColor = {"#0D3B66"}
+  impulseColor = {"#F95738"}
+  relaxationColor ={"#B7D7F6"}
+  />
 }
 
 export default App
