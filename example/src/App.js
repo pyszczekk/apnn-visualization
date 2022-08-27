3bedf59fd63d19ee2f4f8865eb0816ef68f11425
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2022-08-27 21:53:54 +0200

    example updates after backend connection

--- example/src/App.js
+++ example/src/App.js
@@ -5,12 +5,12 @@ import 'apnn-visualization/dist/index.css'
 
 const App = () => {
   return <VisualizationApp 
-  connectionAddress = {"192.168.15.15:13001"} 
-  bgColor = {"#E9DEE9"} 
-  neuronColor = {"#F4D35E"}
-  synapsisColor = {"#0D3B66"}
-  impulseColor = {"#F95738"}
-  relaxationColor ={"#B7D7F6"}
+  // connectionAddress = {"192.168.15.15:13001"} 
+  // bgColor = {"#E9DEE9"} 
+  // neuronColor = {"#F4D35E"}
+  // synapsisColor = {"#0D3B66"}
+  // impulseColor = {"#F95738"}
+  // relaxationColor ={"#B7D7F6"}
   />
 }
 
